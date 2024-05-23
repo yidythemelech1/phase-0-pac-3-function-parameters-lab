803d602c88d8be2a3796076018aff8c0dcee91e8
Yidy <jetfung060@gmail.com>
2024-05-23 17:10:22 -0400

    completed

---
+++ index.js
@@ -0,0 +1,24 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+// Define a function that uses a parameter
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+}
+
+// Define a function that uses two parameters
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+// Define a function that uses an optional parameter
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+  
